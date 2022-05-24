acd1b1028545205471980d935f1b62c4e7e405d6
Sajib Hossain <sajib.sj359@gmail.com>
2022-05-24 18:12:26 +0600

    Added footer section

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from '../Shared/Footer/Footer';
 import Banner from './Banner';
 
 
@@ -9,6 +10,8 @@ const Home = () => {
             <Banner></Banner>
             <h1 className='text-5xl font-bold text-red-400 font-serif my-5'>Computer Manufacturing Industry Limited</h1>
             <p className='text-3xl text-blue-400 font-mono font-bold'>This mainly Manufacture Computer Casing and Power Supply</p>
+
+            <Footer></Footer>
 
         </div>
     );
